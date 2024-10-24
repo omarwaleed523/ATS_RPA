e47f44e88cfa8fb78a22f39964e5bb920e44e65d
Mohamed Tarek Mohamed <angrym21@gmail.com>
2024-10-24 23:24:28 +0300

    Added Send_email for automation

--- get_email_info.py
+++ get_email_info.py
@@ -8,10 +8,10 @@ IMAP_SERVER = 'imap.gmail.com'
 IMAP_PORT = 993
 
 # Email account credentials
-EMAIL_USER = 'omarwaleed5234@gmail.com'
-EMAIL_PASS = 'njwf acix mevh lkgi'  # Use an app-specific password if 2FA is enabled
+EMAIL_USER = 'angrym21@gmail.com'
+EMAIL_PASS = 'zysg szis hdvq kbzo'  # Use an app-specific password if 2FA is enabled
 
-ATTACHMENTS_DIR = 'C:\\Users\\omarw\\PycharmProjects\\rpa_ats\\resume'
+ATTACHMENTS_DIR = 'D:\\me\\Mini Ant\\MSA\\Graduation Project\\Model1\\resumes'
 
 
 
--- main.py
+++ main.py
@@ -6,5 +6,12 @@ def run_get_email_info():
     # Running the other Python file
     subprocess.run(["python", "get_email_info.py"])  # Use "python" if on Windows
 
+
+def send_email():
+    # Running the other Python file
+    subprocess.run(["python", "send_email.py"])  # Use "python" if on Windows
+
+
 if __name__ == "__main__":
-    run_get_email_info()
+    #run_get_email_info()
+    send_email()
